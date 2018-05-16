4d54e8a0198a3166a5877b66e46e512250eddc34
georgefaraj <georgemilio@gmail.com>
2018-05-16 07:32:18 -0300

    Added user input and button. Created button function that updates the text based on user input.

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void updateBox(View view) {
+        TextView output = findViewById(R.id.textView);
+        EditText input = findViewById(R.id.editText);
+        output.setText(input.getText());
     }
 }
